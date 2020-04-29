197c6ddee1cd95376708acca80ea71148079b1ae
Anant Ojha <46407205+anantojha@users.noreply.github.com>
2020-04-28 20:17:37 -0400

    Update rmDupsRecursive.c

--- rmDupsRecursive.c
+++ rmDupsRecursive.c
@@ -9,10 +9,13 @@ int main()
    char src[100];
    char dest[100];
 
+   // enter input string from which to remove duplicates
+   // EXAMPLE:   AAABBABCCABA
    scanf("%s",src);
 
    remov(src, dest, 0);
-
+   
+   // OUTPUT:    ABC
    printf("%s\n", dest);
 
    return 0;
@@ -21,20 +24,24 @@ int main()
 
 
 void remove_Dups_recursively(char *src, char *dest, int iter){
-
+   // base case 1
    if(strlen(src) <= 1){
       dest = src;
       return;
    }
+   
+   // base case 2
    if(iter == strlen(src)){
       return;
    }
 
+   // current 'iter' element has been encountered before
    if(strchr(dest, src[iter]) == NULL){
        dest[strlen(dest)] = src[iter]; 
        iter++;
        remov(src, dest, iter);
    }
+   // current 'iter' element has not been encountered before
    else{
       iter++;
       remov(src, dest, iter);
